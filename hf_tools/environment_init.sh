cb495554df117768c9499f5951731563fcb19fe8
danerli（李润） <lirunchh@gmail.com>
2024-05-15 15:09:11 +0800

    Update environment_init.sh

--- hf_tools/environment_init.sh
+++ hf_tools/environment_init.sh
@@ -13,14 +13,14 @@
 # Check the ID and VERSION_ID and perform the appropriate operation
 if [ "$ID" = "ubuntu" ]; then
     # install aria2
-    sudo apt update && sudo apt install aria2
+    sudo apt update && sudo apt install aria2 -y
     # install git-lfs
     curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-    apt-get install git-lfs
+    apt-get install git-lfs -y
 
 elif [ "$ID" = "centos" ]; then
     # install aria2
-    sudo yum update && sudo yum install epel-release aria2
+    sudo yum update && sudo yum install epel-release -y && sudo yum install aria2 -y
     # install git-lfs
     curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | sudo os=el dist=$VERSION_ID bash
     yum install git-lfs -y
