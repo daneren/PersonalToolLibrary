5a2d0dd6ccb824d7739a5b029fa61df1d02a3f88
danerli（李润） <lirunchh@gmail.com>
2025-03-12 17:05:03 +0800

    Create costum_commands.sh

---
+++ costum_commands.sh
@@ -0,0 +1,11 @@
+
+# setting DEFALULT_COS_CONF_PATH
+export DEFALULT_COS_CONF_PATH="~/.cos/ti_danerli_shanghai.conf"
+
+# coscmd command
+alias cdowns="coscmd -c $DEFALULT_COS_CONF_PATH download -rf "
+alias cups="coscmd -c $DEFALULT_COS_CONF_PATH upload -rf "
+alias cdown="coscmd -c $DEFALULT_COS_CONF_PATH download -f "
+alias cup="coscmd -c $DEFALULT_COS_CONF_PATH upload -f "
+alias cdowns_pwd="coscmd -c $DEFALULT_COS_CONF_PATH download -rf $PWD $PWD"
+alias cups_pwd="coscmd -c $DEFALULT_COS_CONF_PATH upload -rf $PWD $PWD"
